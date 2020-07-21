303791a9b4d133ae0759b0df8d1889812485685a
Tp-13 <66135230+Tp-13@users.noreply.github.com>
2020-07-21 11:28:59 -0400

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,20 @@
+class Block extends BaseClass {
+    constructor(x, y){
+      super(x,y,20,20);
+      this.visibility = 255;
+    }
+  
+    display(){
+      fill("teal");
+      if (this.body.speed < 3){
+        super.display();
+      }
+      else{
+        World.remove(world, this.body);
+        push();
+        this.visibility = this.visibility - 5;
+        tint(255,this.visibility)
+        pop();
+    }
+  };
+}  
